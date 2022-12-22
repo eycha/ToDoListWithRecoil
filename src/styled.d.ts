e2f98f15c7dab5fe3939fd65a5cd2742a7624d01
eycha <silvlexonst@gmail.com>
2022-12-22 18:01:37 +0900

    Recoil refactoring

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,15 +2,9 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    red: string;
-    black: {
-      veryDark: string;
-      darker: string;
-      lighter: string;
-    };
-    white: {
-      darker: string;
-      lighter: string;
-    };
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+    cardBgColor: string;
   }
 }
