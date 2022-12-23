750532a640aef155d1de32ce0e2a7d4d688bdcea
eycha <silvlexonst@gmail.com>
2022-12-23 12:16:21 +0900

    Immutability adding

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,28 @@
 import { IToDo, toDoState } from "../atoms";
 import { useSetRecoilState } from "recoil";
 import React from "react";
+
+const food = ["pizza", "mango", "kimchi", "kimbab"]
+const front = ["pizza"]
+const back = ["kimchi", "kimbab"]
+const finalPart = [...front, "감", ...back]
+console.log(finalPart)
+
  
 function ToDo({ text, category, id }: IToDo) {
     const setToDos = useSetRecoilState(toDoState);
     const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
         const { currentTarget: { name },
-    } = event
+      } = event
+      
+      setToDos((oldToDos) => {
+        const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id)
+        const newToDos = {text, id, category: name as any};
+        return [...oldToDos.slice(0, targetIndex),
+          newToDos,
+        ...oldToDos.slice(targetIndex + 1),
+        ];
+      });
  };
   return (
     <li>
